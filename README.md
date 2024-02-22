79af818cc359a33a4e8735cbe05d4fd2db67d67e
B0yb0l0 <omongaldiyevboburbek@gmail.com>
2024-02-22 17:37:24 +0500

    portfoliyo

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfoliyo
+# portfoliyo
---
+++ main.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".text",{
+    strings:["Frontend Developer", "Bloger", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+// let client = true
+// let discount = client ? '25%' : '5%';
+// console.log('Discount:', discount);
